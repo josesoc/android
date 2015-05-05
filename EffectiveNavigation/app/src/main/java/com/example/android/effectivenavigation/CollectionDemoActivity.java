5a914e31cd272862794beef99478d81070333b8c
josesoc <josesocialnetworks@gmail.com>
2015-05-05 09:28:20 +0200

    Toolbars, google-course update ....

--- EffectiveNavigation/app/src/main/java/com/example/android/effectivenavigation/CollectionDemoActivity.java
+++ EffectiveNavigation/app/src/main/java/com/example/android/effectivenavigation/CollectionDemoActivity.java
@@ -19,6 +19,12 @@ package com.example.android.effectivenavigation;
 import android.app.ActionBar;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
@@ -26,11 +32,6 @@ import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.app.NavUtils;
 import android.support.v4.app.TaskStackBuilder;
 import android.support.v4.view.ViewPager;
-import android.view.LayoutInflater;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
 
 public class CollectionDemoActivity extends FragmentActivity {
 
