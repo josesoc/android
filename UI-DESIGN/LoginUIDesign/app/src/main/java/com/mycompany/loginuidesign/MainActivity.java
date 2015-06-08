e2c514780541d1de41442f40775aadda02d0a744
josesoc <josesocialnetworks@gmail.com>
2015-06-08 13:24:06 +0200

    UI-DESIGN example, StylesThemesExample new gradient button, Search  that is not completed

---
+++ UI-DESIGN/LoginUIDesign/app/src/main/java/com/mycompany/loginuidesign/MainActivity.java
@@ -0,0 +1,51 @@
+package com.mycompany.loginuidesign;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+/**
+ * https://www.youtube.com/watch?v=XKBJKS84UYI
+ */
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void forgetYourPassword(View view) {
+        Toast.makeText(getApplicationContext(), "Forget your password", Toast.LENGTH_SHORT).show();
+    }
+
+    public void login(View view) {
+        Toast.makeText(getApplicationContext(), "Login", Toast.LENGTH_SHORT).show();
+    }
+}
